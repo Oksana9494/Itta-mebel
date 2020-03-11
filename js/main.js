9876d56d0e29a790e1ac0a9abd53c8a0a138ea27
Oksana9494 <o90494@yandex.ru>
2020-03-11 09:45:58 +0300

    HTML-code of the website
    page layout
    add sliders

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $('.block-slider').slick({
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+});
+$(document).ready(function(){
+    $('.project-carousel').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+    });
+});
+$(document).ready(function(){
+    $('.facts-slider').slick({
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        //autoplay: true,
+        //autoplaySpeed: 3000,
+    });
+});
