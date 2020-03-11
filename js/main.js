be1e39bb51bf1dcfa706df18f5485ef5db1cfb9d
Oksana9494 <o90494@yandex.ru>
2020-03-11 15:58:49 +0300

    add button

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
         arrows: false,
         slidesToShow: 1,
         slidesToScroll: 1,
-        //autoplay: true,
-        //autoplaySpeed: 3000,
+        autoplay: true,
+        autoplaySpeed: 3000,
     });
 });
